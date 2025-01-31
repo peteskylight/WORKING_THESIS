0b835160ced5b44bd56c91a6e332cb09106b53ac
Bennett <peterjustindatingaling.main@gmail.com>
2025-01-31 15:15:12 +0800

    Checkpoint for restructuring application.
    This is to address the issue of high RAM usage. Let's check if this is effective

---
+++ RESTRUCTURED_APPLICATION/gui/main_window.py
@@ -0,0 +1,357 @@
+import sys
+import cv2
+import os
+import numpy as np
+import psutil
+import GPUtil
+from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox, QTableWidgetItem, QWidget, QButtonGroup
+from PySide6.QtCore import QRect, QCoreApplication, QMetaObject, QTimer, QTime, Qt, QDate
+
+from ultralytics import YOLO
+
+from utils.drawing_utils import DrawingUtils
+from gui import Ui_MainWindow
+from trackers import PoseDetection
+from utils import VideoProcessor, Tools, VideoUtils
+from gui_commands import (CenterVideo,
+                          FrontVideo,
+                          CreateDataset,
+                          AnalyticsTab)
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        
+        self.setupUi(self)
+
+        #==== Create INSTANCES =========
+        
+        self.CenterVideo = CenterVideo(main_window=self)
+        self.FrontVideo = FrontVideo(main_window=self)
+        self.CreateDataset = CreateDataset(main_window=self)
+        self.AnalyticsTab = AnalyticsTab(main_window=self)
+        
+        self.drawing_utils = DrawingUtils()
+        self.tools_utils = Tools()
+        self.video_utils = VideoUtils()
+        
+        self.videoWidth = None
+        self.videoHeight = None
+
+        self.fps_flider_value = 30
+        self.returned_frames_from_browsed_center_video = None
+        self.returned_frames_from_browsed_front_video = None
+
+        self.cropped_frame_for_front_video_analytics_preview = None
+        self.cropped_frame_for_center_video_analytics_preview = None
+        
+        self.analyticsTab_index = 1
+        self.createDatasetTab_index = 2
+
+        self.createDataset_tab = self.MainTab.widget(self.createDatasetTab_index)  # Index of the tab you want to hide
+        self.createDataset_tab_index = self.createDatasetTab_index  # Index of the tab you want to hide
+        self.createDataset_tab_title = self.MainTab.tabText(self.createDataset_tab_index)
+
+        # Initially hide the tab
+        self.MainTab.removeTab(self.createDataset_tab_index)
+        
+        self.analytics_tab = self.MainTab.widget(self.analyticsTab_index) 
+        self.analytics_tab_index = self.analyticsTab_index
+        self.analytics_tab_title = self.MainTab.tabText(self.analytics_tab_index)
+
+        # Initially hide the tab
+        self.MainTab.removeTab(self.analytics_tab_index)
+        
+        # Connect the QAction's triggered signal to the toggle_createDataset_tab method
+        self.actionviewCreateDataset.triggered.connect(self.toggle_createDataset_tab)
+
+        #============ FOR IMPORTING VIDEO TAB ===========
+        
+        #Create INSTANCES\
+        self.human_detect_model = YOLO("yolov8n.pt")
+        self.human_pose_model = YOLO("yolov8n-pose.pt")
+        self.human_detect_conf = 0.5
+        self.human_pose_conf = 0.5
+        
+        self.human_pose_detection = PoseDetection(humanDetectionModel="yolov8n.pt",
+                                                  humanDetectConf=self.human_detect_conf,
+                                                  humanPoseModel="yolov8n-pose.pt",
+                                                  humanPoseConf=self.human_pose_conf)
+        
+        self.human_detect_results = None
+        self.video_processor = None
+        self.center_white_frames_preview = None
+        self.front_white_frames_preview = None
+
+        self.human_detection_results = []
+        
+        self.is_center_video_playing = False
+        self.is_front_video_playing = False
+
+        self.frame_processing_value = None
+        
+        self.import_video_button_center.clicked.connect(self.CenterVideo.browse_video)
+        self.play_pause_button_video_center.clicked.connect(self.CenterVideo.toggle_play_pause)
+
+        self.import_video_button_front.clicked.connect(self.FrontVideo.browse_video)
+        self.play_pause_button_video_front.clicked.connect(self.FrontVideo.toggle_play_pause)
+
+        #=====GET FOOTAGE ANALYTICS
+        self.are_videos_ready = False
+        self.whole_classroom_height = 1080
+        self.front_starting_y = 750
+        self.center_starting_y = 100
+
+        self.analyze_video_button.clicked.connect(self.switch_to_analytics_tab)
+
+
+        #Adjust this according to video but meh. This is just the default. Just check the specs of the cam. Default naman sya all the times
+        self.center_video_interval = 1000//30 #30 fps
+        self.front_video_interval = 1000//30 #30 fps
+
+        self.clock_interval = 1000  # 1 second interval for clock
+        self.toggle_record_label_interval = 750
+
+        self.center_video_counter = 0
+        self.front_video_counter = 0
+        self.cropped_center_video_counter = 0
+        self.cropped_front_video_counter = 0
+
+        
+        self.clock_counter = 0
+        self.toggle_record_label_counter = 0
+        self.toggle_import_indicator = 0
+        self.center_video_frame_counter = 0        
+        self.front_video_frame_counter = 0
+
+        self.cropped_center_video_frame_counter = 0
+        self.cropped_front_video_frame_counter = 0
+
+        
+        #============ FOR CREATE DATASET TAB ============
+        
+        #Disable Maximize Button
+        self.setWindowFlag(Qt.WindowMaximizeButtonHint, False)
+
+        self.closeCamera.clicked.connect(self.CreateDataset.stop_camera)
+        self.openCamera.clicked.connect(self.CreateDataset.start_camera)
+        self.browseButton.clicked.connect(self.browse_button_functions)
+        self.refresh_button.clicked.connect(lambda: self.CreateDataset.scan_directory(self.directoryLineEdit.text()))
+        self.add_action_button.clicked.connect(self.CreateDataset.add_folder)
+        self.delete_action_button.clicked.connect(self.CreateDataset.delete_folder)
+        self.recording_button.clicked.connect(self.toggle_button)
+        self.refresh_action_list.clicked.connect(self.CreateDataset.showActionsToTable)
+        self.CreateDataset.populate_camera_combo_box()
+
+        # Slider Value Change
+        self.interval_slider.valueChanged.connect(self.updateIntervalLabel)
+        self.sequence_slider.valueChanged.connect(self.updateSequenceLabel)
+        
+        # Set Column Names
+        self.action_table.setHorizontalHeaderLabels(["Actions", "# of Recordings"])
+        
+        # Center the window on the screen
+        self.center()
+        
+        #Set the date:
+        current_date = QDate.currentDate().toString()
+        
+        self.day_label.setText(f"{current_date}")
+        
+        self.timer = QTimer(self) #TIMER FOR ALL!!!
+        self.timer.timeout.connect(self.update_all_with_timers) #=======Update all and just have conditional statements
+        self.timer.start(10) 
+        
+    def update_all_with_timers(self):
+        
+        self.clock_counter += self.timer.interval()
+        self.toggle_record_label_counter += self.timer.interval()
+        
+        if self.is_center_video_playing:
+            self.center_video_counter += self.timer.interval()
+            if (self.center_video_counter >= self.center_video_interval):
+                if self.center_video_frame_counter >= len(self.returned_frames_from_browsed_center_video):
+                    self.center_video_frame_counter = 0 #Resets the playing of video
+                
+                if self.keypointsOnlyChkBox_Center.isChecked():
+                    self.CenterVideo.update_white_frame(self.center_white_frames_preview[self.center_video_frame_counter])
+                else:
+                    self.CenterVideo.update_frame(self.returned_frames_from_browsed_center_video[self.center_video_frame_counter])
+                
+                self.center_video_counter = 0
+                self.center_video_frame_counter += 1
+        
+        if self.is_front_video_playing:
+            self.front_video_counter += self.timer.interval()
+            if (self.front_video_counter >= self.front_video_interval):
+                if self.front_video_frame_counter >= len(self.returned_frames_from_browsed_front_video):
+                    self.front_video_frame_counter = 0 #Resets the playing of video
+                
+                if self.keypointsOnlyChkBox_front.isChecked():
+                    self.FrontVideo.update_white_frame(self.front_white_frames_preview[self.front_video_frame_counter])
+                else:
+                    self.FrontVideo.update_frame(self.returned_frames_from_browsed_front_video[self.front_video_frame_counter])
+                
+                self.front_video_counter = 0
+                self.front_video_frame_counter += 1
+        
+        #For cropped analytics
+        if self.are_videos_ready:
+            self.cropped_front_video_counter += self.timer.interval()
+            if (self.cropped_front_video_counter >= self.center_video_interval):
+                #if the counter for center video frame exceeds or is equal to the length of the frame list,
+                #it resets the videod
+                if self.cropped_front_video_frame_counter >= len(self.returned_frames_from_browsed_front_video):
+                    self.cropped_front_video_frame_counter = 0 #it resets here haha
+                    self.cropped_center_video_frame_counter = 0 #it resets here haha
+                
+                #Insert here the code for updating frame for each video
+                #the 2 videos must have same 
+
+                #For front view
+                self.AnalyticsTab.update_frame_for_front_video_label(self.returned_frames_from_browsed_front_video[self.cropped_front_video_frame_counter],
+                                                                    starting_y= self.front_starting_y,
+                                                                    whole_classroom_height=self.whole_classroom_height
+                                                                    )
+                
+                #For center view
+                self.AnalyticsTab.update_frame_for_center_video_label(self.returned_frames_from_browsed_center_video[self.cropped_center_video_frame_counter],
+                                                                    center_starting_y=self.center_starting_y,
+                                                                    front_starting_y=self.front_starting_y,
+                                                                    )
+
+                self.cropped_front_video_counter = 0 #reset the interval haha
+                self.cropped_center_video_counter = 0
+
+                self.cropped_front_video_frame_counter += 1 #increment to proceed to next frame lol
+                self.cropped_center_video_frame_counter += 1 #increment to proceed to next frame lol
+                
+
+
+
+        if self.clock_counter >= self.clock_interval:
+            self.CreateDataset.update_usage()
+            self.CreateDataset.update_time()
+            self.clock_counter = 0
+
+        if self.toggle_record_label_counter >= self.toggle_record_label_interval:
+            self.toggleLabelVisibility()
+            self.toggle_record_label_counter = 0
+        
+    
+            
+    def center(self):
+        # Get the screen geometry
+        screen = QApplication.primaryScreen()
+        screen_geometry = screen.availableGeometry()
+        # Get the window geometry
+        window_geometry = self.frameGeometry()
+        # Move the window to the center of the screen
+        window_geometry.moveCenter(screen_geometry.center())
+        self.move(window_geometry.topLeft())
+
+    
+
+    def browse_button_functions(self):
+        self.CreateDataset.open_file_explorer()
+        self.CreateDataset.showActionsToTable()
+
+    def startBlinking(self):
+        self.status_label.setStyleSheet("""
+                QLabel {
+                    color: rgb(0, 255, 0);
+                }
+            """)
+        self.status_label.setVisible(True) # Ensure the label is visible when starting
+    
+    def stopBlinking(self):
+        self.status_label.setStyleSheet("""
+                QLabel {
+                    color: rgb(0, 255, 0);
+                }
+            """)
+        self.blink_timer.stop()
+        self.status_label.setText("NOT RECORDING")
+        self.status_label.setVisible(True)
+        
+    def toggleLabelVisibility(self):
+        self.status_label.setVisible(not self.status_label.isVisible())
+        
+    def updateIntervalLabel(self, value):
+        self.interval_label.setText(str(value))
+        
+    def updateSequenceLabel(self, value):
+        self.sequence_label.setText(str(value))
+        
+    def toggle_createDataset_tab(self):
+        # Toggle the visibility of the tab
+        if self.createDataset_tab_index == -1 or not self.MainTab.isTabVisible(self.createDataset_tab_index):
+            # Add the tab back
+            self.MainTab.addTab(self.createDataset_tab, self.createDataset_tab_title)
+            self.createDataset_tab_index = self.MainTab.indexOf(self.createDataset_tab)
+            self.MainTab.setCurrentIndex(self.createDataset_tab_index)
+        else:
+            # Remove the tab
+            self.MainTab.removeTab(self.createDataset_tab_index)
+            self.createDataset_tab_index = -1
+    
+    def toggle_analytics_tab(self):
+        # Toggle the visibility of the tab
+        if self.analytics_tab_index == -1 or not self.MainTab.isTabVisible(self.analytics_tab_index):
+            # Add the tab back
+            self.MainTab.addTab(self.analytics_tab, self.analytics_tab_title)
+            self.analytics_tab_index = self.MainTab.indexOf(self.analytics_tab)
+            self.MainTab.setCurrentIndex(self.analytics_tab_index)
+        else:
+            # Remove the tab
+            self.MainTab.removeTab(self.analytics_tab_index)
+            self.analytics_tab_index = -1
+
+    def toggle_button(self):
+        if self.recording_button.text() == "START\nRECORDING":
+            self.recording_button.setText("STOP\nRECORDING")
+            self.recording_button.setStyleSheet("""
+                QPushButton {
+                    background-color: rgb(170, 0, 0);
+                    border-radius: 15px; /* Adjust the radius as needed */
+                    color: black; /* Set the text color */
+                    border: 1px solid black; /* Optional: Add a border */
+                }
+            """)
+            self.status_label.setText("RECORDING")
+            self.startBlinking()
+            
+        else:
+            self.recording_button.setText("START\nRECORDING")
+            self.recording_button.setStyleSheet("""
+                QPushButton {
+                    background-color: rgb(170, 255, 127);
+                    border-radius: 15px; /* Adjust the radius as needed */
+                    color: black; /* Set the text color */
+                    border: 1px solid black; /* Optional: Add a border */
+                }
+            """)
+            self.status_label.setText("NOT RECORDING")
+    
+    
+    #template for warning message boxes:
+    def show_warning_message(self, status_title, message):
+        # Create and show a warning message box
+        msg_box = QMessageBox()
+        msg_box.setIcon(QMessageBox.Warning)
+        msg_box.setWindowTitle(status_title)
+        msg_box.setText(message)
+        msg_box.setStandardButtons(QMessageBox.Ok)
+        msg_box.exec()
+
+    #Switch to analytics tab
+    def switch_to_analytics_tab(self):
+        if (self.returned_frames_from_browsed_center_video and self.returned_frames_from_browsed_front_video) is not None:
+            self.toggle_analytics_tab()
+            self.MainTab.setCurrentIndex(1)
+            self.are_videos_ready = True
+            self.play_pause_button_analytics.setEnabled(True)
+            self.play_pause_button_analytics.setText("PAUSE")
+        else:
+            self.show_warning_message(status_title="Error!",
+                                      message= "Please import a complete set of footages.")
---
+++ RESTRUCTURED_APPLICATION/trackers/pose_detection.py
@@ -0,0 +1,224 @@
+from ultralytics import YOLO
+import cv2
+import numpy as np
+import sys
+import os
+
+from PySide6.QtCore import QThread, Signal
+sys.path.append('../')
+
+from ultralytics.utils.plotting import Annotator
+from utils import DrawingUtils
+
+class PoseDetection:
+    def __init__(self, humanDetectionModel, humanDetectConf, humanPoseModel, humanPoseConf):
+        self.human_detection_model = YOLO(humanDetectionModel)
+        self.human_detection_conf = humanDetectConf
+        self.human_pose_model = YOLO(humanPoseModel)
+        self.human_pose_conf = humanPoseConf
+    
+        self.drawing_utils = DrawingUtils()
+
+    def getModel(self, frame, model, confidenceRate):
+        # Recolor Feed from RGB to BGR
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+        
+        # Perform inference using the YOLO model
+        results = model(frame, conf = confidenceRate)
+
+        # Recolor image back to BGR for rendering
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        
+        return results
+    
+    def flatten_keypoints(self, human_pose_results):
+        normalized_keypoints =  np.array(human_pose_results[0].keypoints.xyn.cpu().numpy()[0])
+        flattenedKeypoints = normalized_keypoints.flatten()
+        return flattenedKeypoints, normalized_keypoints
+    
+    def getHumanPoseKeypoints(self, frame):
+        
+        return_bbox = None
+        
+        human_pose_results = self.getModel(frame = frame,
+                                            model = self.human_pose_model,
+                                            confidenceRate = self.human_pose_conf)
+        
+        flatten_keypoints, normalized_keypoints = self.flatten_keypoints(human_pose_results=human_pose_results)
+
+        for result in human_pose_results:
+            bboxes = result.boxes.xyxy
+            
+            for bbox in bboxes:
+                return_bbox = bbox
+        
+        return frame, normalized_keypoints, return_bbox
+
+
+    def getHumanTrackResults(self, frame, model, confidenceRate):
+        # Recolor Feed from RGB to BGR
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+        
+        # Perform inference using the YOLO model
+        results = model.track(image,
+                              conf = confidenceRate,
+                              persist=True,
+                              classes=0,
+                              iou=0.5,
+                              agnostic_nms=True)[0]
+        id_name_dict = results.names
+
+        # Recolor image back to BGR for rendering
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        
+        return results, id_name_dict
+    
+    def flatten_keypoints(self, human_pose_results):
+        normalized_keypoints =  np.array(human_pose_results[0].keypoints.xyn.cpu().numpy()[0])
+        flattenedKeypoints = normalized_keypoints.flatten()
+        return flattenedKeypoints, normalized_keypoints
+    
+
+
+class HumanDetectionThread(QThread):
+    human_track_results = Signal(object)
+    human_detection_progress_update = Signal(object)
+
+    def __init__(self, video_frames, isFront=True, humanDetectionModel='yolov8n.pt', humanDetectConf=0.5):
+        super().__init__()
+        self.video_frames = video_frames  # List of frames to process
+        self.isFront = isFront
+        self.human_detection_model = YOLO(humanDetectionModel)
+        self.human_detection_confidence = humanDetectConf
+        self.student_detections_dicts = []
+        self._running = True
+
+    def create_roi_mask(self, frame, row_height):
+        height, width, _ = frame.shape
+        mask = np.zeros((height, width), dtype=np.uint8)
+        roi = (0, row_height, width, height)  # Bottom to middle
+        cv2.rectangle(mask, (roi[0], roi[1]), (roi[2], roi[3]), 255, -1)
+        cv2.line(img=frame, pt1=(0, row_height), pt2=(frame.shape[1], row_height),color = (0,255,0), thickness=2)
+        return mask
+
+    def run(self):
+        total_frames = len(self.video_frames)
+        current_frame = 0
+        id_name_dict = None
+        student_dict = None
+        results = None
+
+        for frame in self.video_frames:
+
+            if not self._running:
+                break
+
+            height, width, _ = frame.shape
+            initial_row_height = int(height * (4 / 6))  # Bottom 4 rows (adjust as needed)
+
+            if self.isFront:
+                roi_mask = self.create_roi_mask(frame, initial_row_height)
+                masked_frame = cv2.bitwise_and(frame, frame, mask=roi_mask)
+                results = self.human_detection_model.track(masked_frame,
+                                                  conf = self.human_detection_confidence,
+                                                  persist=True,
+                                                  classes=0,
+                                                  iou=0.3,
+                                                  agnostic_nms=True)[0]
+                id_name_dict = results.names
+                student_dict = {}
+            else:
+                results = self.human_detection_model.track(frame,
+                                                  conf = self.human_detection_confidence,
+                                                  persist=True,
+                                                  classes=0,
+                                                  iou=0.3,
+                                                  agnostic_nms=True)[0]
+                id_name_dict = results.names
+                student_dict = {}
+
+            output_frame = frame.copy()
+
+            for result in results:
+                boxes = result.boxes
+                for box in boxes:
+                    #Get the image per person
+                    b = box.xyxy[0]
+                    c = box.cls
+                    cropped_image = frame[int(b[1]):int(b[3]), int(b[0]):int(b[2])]
+                    
+                    if box.id is not None and box.xyxy is not None and box.cls is not None:
+                        track_id = int(box.id.tolist()[0])
+                        track_result = box.xyxy.tolist()[0]
+                        object_cls_id = box.cls.tolist()[0]
+                        object_cls_name = id_name_dict.get(object_cls_id, "unknown")
+                        if object_cls_name == "person":
+                            student_dict[track_id] = track_result
+                    else:
+                        print("One of the attributes is None:", box.id, box.xyxy, box.cls)
+            
+            self.student_detections_dicts.append(student_dict)
+            current_frame += 1
+            progress = int((current_frame / total_frames) * 100)
+            self.human_detection_progress_update.emit(progress)
+        
+        self.human_track_results.emit(self.student_detections_dicts)
+
+    def stop(self):
+        self._running = False
+        self.wait()
+
+class PoseDetectionThread(QThread):
+    pose_detection_results = Signal(object)
+    pose_detection_progress_update = Signal(object)
+    
+    def __init__(self, original_frames, human_detect_results, humanDetectionModel, humanDetectConf, humanPoseModel, humanPoseConf):
+        super().__init__()
+        
+        self.human_detection_model = YOLO(humanDetectionModel)
+        self.human_detection_confidence = humanDetectConf
+        self.human_pose_model = YOLO(humanPoseModel)
+        self.human_pose_confidence = humanPoseConf
+        
+        self.original_frames = original_frames
+        self.human_detect_results = human_detect_results
+        self._running = True
+    
+    def run(self):
+        total_frames = len(self.human_detect_results)
+        current_frame = 0
+        pose_results_list = []
+        
+        for frame, detection in zip(self.original_frames, self.human_detect_results):
+            keypoints_dict = {}  # Initialize here to store keypoints for all detections in the frame
+            for track_id, bbox in detection.items():
+                # Extract the bounding box coordinates
+                x1, y1, x2, y2 = map(int, bbox)
+                cropped_image = frame[y1:y2, x1:x2]
+ 
+                try:
+                    # Perform pose detection on the cropped image
+                    results = self.human_pose_model(cropped_image, self.human_pose_confidence)
+                    for result in results:
+                        if result.keypoints:
+                            keypoints_normalized = np.array(result.keypoints.xyn.cpu().numpy()[0])
+                            keypoints_dict[track_id] = keypoints_normalized
+                        else:
+                            print(f"Error processing track ID {track_id}: {e}")
+                except Exception as e:
+                    print(f"Error processing track ID {track_id}: {e}")
+            
+            pose_results_list.append(keypoints_dict)
+            current_frame += 1
+            progress = int((current_frame / total_frames) * 100)
+            self.pose_detection_progress_update.emit(progress)
+        
+        self.pose_detection_results.emit(pose_results_list)
+        
+    def stop(self):
+        self._running = False
+        self.wait()
